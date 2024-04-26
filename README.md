e1363c5da3972db28132ed6e71a7cb890238c3f0
lining <770677655@qq.com>
2024-04-26 17:58:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# 计算机图形学编程(使用OpenGL和C++)(第2版)
+
+    OpenGL4.3及以上版本    虚拟机的ubuntu在使用的时候，需要VMware17版本(支持OpenGL4.3)，ubuntu22.04版本及以上
+    SOIL2 地址 https://github.com/SpartanJ/SOIL2
+    注意这个SOIL2的库，就要用libs/SOIL2下的，从github上下载的1-20，有问题
+
+## chapter2
+
+    
---
+++ chapter2/files/main.cpp
@@ -0,0 +1,92 @@
+//
+// Created by lining on 2024/4/26.
+//
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+string readFile(string filePath) {
+    string content;
+    ifstream fileStream(filePath, ios::in);
+    if (!fileStream.is_open()) {
+        throw "Unable to open file.";
+    }
+    string line = "";
+    while (!fileStream.eof()) {
+        getline(fileStream, line);
+        content.append(line + "\n");
+    }
+    fileStream.close();
+    return content;
+}
+
+const string basePath = "chapter2/files/";
+
+GLuint createShaderProgram() {
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    GLuint vfprogram = glCreateProgram();
+
+    string vertShaderStr = readFile(basePath + "vertShader.glsl");
+    string fragShaderStr = readFile(basePath + "fragShader.glsl");
+    const char *vertShaderSrc = vertShaderStr.c_str();
+    const char *fragShaderSrc = fragShaderStr.c_str();
+
+    glShaderSource(vShader, 1, &vertShaderSrc, NULL);
+    glShaderSource(fShader, 1, &fragShaderSrc, NULL);
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    glAttachShader(vfprogram, vShader);
+    glAttachShader(vfprogram, fShader);
+    glLinkProgram(vfprogram);
+
+    return vfprogram;
+}
+
+void init(GLFWwindow *window) {
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow *window, double currentTime) {
+    glUseProgram(renderingProgram);
+    glPointSize(30.0f);
+    glDrawArrays(GL_POINTS, 0, 1);
+}
+
+int main(void) {
+    if (!glfwInit()) { exit(EXIT_FAILURE); }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(600, 600, "Chapter 2 - program 4", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+    glfwSwapInterval(1);
+
+    init(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
